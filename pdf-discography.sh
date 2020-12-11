a05a9fe1278e48c70b07d4845710b45ae666dc7a
Daeduek Cho <daduki@gmail.com>
2020-12-11 13:54:57 +0900

    renaming

--- pdf-discography.sh
+++ pdf-discography.sh
@@ -2,7 +2,7 @@ echo 'Killing all Jekyll instances'
 kill -9 $(ps aux | grep '[j]ekyll' | awk '{print $2}')
 clear
 
-echo "Building PDF-friendly HTML site for Product1 ...";
+echo "Building PDF-friendly HTML site for Discography ...";
 bundle exec jekyll serve --detach --config _config.yml,pdfconfigs/config_discography_pdf.yml;
 echo "done";
 
